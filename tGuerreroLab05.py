c2ad2610621a86c48ed9af188093da2007e24b43
tedguerrero <tedguerrerojr@gmail.com>
2026-08-20 01:24:47 +0000

    Lab 5

---
+++ tGuerreroLab05.py
@@ -0,0 +1,49 @@
+#Ted Guerrero
+#CTC389
+#Lab 5, Lists
+
+nums = [10,20,30,40,50,60,70,80,90,100]
+print("\n")
+
+print("No Loop")
+print(nums[0])
+print("\n")
+
+print(nums[1])
+print("\n")
+
+print(nums[2])
+print("\n")
+
+print(nums[3])
+print("\n")
+
+print(nums[4])
+print("\n")
+
+print(nums[5])
+print("\n")
+
+print(nums[6])
+print("\n")
+
+print(nums[7])
+print("\n")
+
+print(nums[8])
+print("\n")
+
+print(nums[9])
+print("\n")
+
+print("With Loop")
+for i in nums:
+    print(i)
+    ("\n")
+
+print("\n")
+
+fruit = ["watermelon","cantelope","pineapple","mango","guava"]
+for i in fruit:
+    print(i)
+    ("\n")
